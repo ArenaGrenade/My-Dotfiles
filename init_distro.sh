146ca270e5eee73575d1b6a470da0552183c4040
ArenaGrenade <rohan.asokan@students.iiit.ac.in>
2022-02-04 17:19:14 +0400

    Added DE Init Scripts

--- init_distro.sh
+++ init_distro.sh
@@ -1,3 +1,4 @@
 echo "Welcome" $SUDO_USER "to the Desktop Setup Process"
 echo "Initializaing system and installing necessary packages - add packages in packages.txt file to install them too"
 source ./install_packages.sh
+echo "Compelted Package Installs"
